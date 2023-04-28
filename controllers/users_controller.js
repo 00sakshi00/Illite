4aa6cb4517215796c095eab4214569ad33741b2d
Sakshi Shukla <92930551+00sakshi00@users.noreply.github.com>
2023-04-28 16:41:06 +0530

    Authenticate

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,26 @@ module.exports.profile= function(req,res){
         title: 'User Profile'
     })
 }
+
+module.exports.signIn= function(req,res){
+    return res.render('sign-in', {
+        title: 'SignIn'
+    })
+}
+
+module.exports.signUp= function(req,res){
+    return res.render('sign-up', {
+        title: 'SignUp'
+    })
+}
+
+// get the sign up data
+module.exports.create = function(req, res){
+    // TODO later
+}
+
+
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    // TODO later
+}
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const usersConrtoller= require('../controllers/users_controller'); 
 
 router.get('/profile',usersConrtoller.profile);
+router.get('/sign-in',usersConrtoller.signIn);
+router.get('/sign-up',usersConrtoller.signUp);
 
 module.exports = router;
