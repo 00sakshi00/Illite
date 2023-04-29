59c500101bfabfdd8e96b70707a2608ff1526bf3
Sakshi Shukla <92930551+00sakshi00@users.noreply.github.com>
2023-04-29 22:21:43 +0530

    .

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,6 +10,9 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up', {
         title: "Sign Up"
     })
@@ -18,6 +21,9 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in', {
         title: "Sign In"
     })
