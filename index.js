49a897490e10e364a26bda369a2b2950e1ddf425
Sakshi Shukla <92930551+00sakshi00@users.noreply.github.com>
2023-08-30 01:41:35 +0530

    .

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
-// connect-mongo@3
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
 
